9e600426ee4c28eb0c575ee8b1f8b99ad53b31ac
Ashley Pieper <ashleypieper@Ashleys-MacBook-Pro.local>
2020-05-05 10:40:23 -0500

    create models

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Pizza):
+    """A type of pizza."""
+    name = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return pizza names."""
+        return self.name
+
+class Entry(models.Topping):
+    """Topings on a pizza."""
+    pizza = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return pizza type & topping name."""
+        return self.name
