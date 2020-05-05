3a5499731c8a9732a2c4aa6b1e58395c4ca997bd
Ashley Pieper <ashleypieper@Ashleys-MacBook-Pro.local>
2020-05-05 12:42:02 -0500

    topics view

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 
 def index(request):
     """The home page for pizzas."""
     return render(request, )
+
+def pizzas(request):
+    """Show all pizzas"""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
